6a143e0b9d2d9c4694fb19162c1880d25d436679
nasimmhn <nasim.mahzoun@gmail.com>
2020-04-24 13:41:57 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react'
+// Styled-components
+import styled from 'styled-components/macro'
+
+// From Components
+import { Input } from './components/Input'
+
+
+
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <MainContainer >
+      <Input />
+    </MainContainer >
   )
 }
+
+
+const MainContainer = styled.div`
+  padding-top: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
+  height: 100%;
+  
+`
