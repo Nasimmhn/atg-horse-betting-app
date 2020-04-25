ae8bad74f3f221347af446035aa40ae027deb98e
nasimmhn <nasim.mahzoun@gmail.com>
2020-04-25 16:43:09 +0200

    set layout

--- src/App.js
+++ src/App.js
@@ -2,27 +2,48 @@ import React from 'react'
 // Styled-components
 import styled from 'styled-components/macro'
 
-// From Components
+// Components
 import { Input } from './components/Input'
-
-
 
 
 export const App = () => {
   return (
-    <MainContainer >
-      <Input />
-    </MainContainer >
+    <AppContainer>
+      <Header> Header </Header>
+      <MainContainer >
+        <Input />
+      </MainContainer >
+      <Footer> Footer </Footer>
+    </AppContainer>
   )
 }
 
+const AppContainer = styled.div`
+  margin: 0px;
+  padding: 0px;
+  min-height: 100vh;
+  background: lightskyblue;
+  display: grid;
+  grid-template-rows: 100px auto 100px;
+  grid-template-areas:
+    "head"
+    "main"
+    "foot";
+`
 
-const MainContainer = styled.div`
+const Header = styled.header`
+  grid-area: head;
+  background: lightyellow;
+`
+const Footer = styled.footer`
+  grid-area: foot;
+  background: lightslategray;
+`
+
+const MainContainer = styled.main`
+  grid-area: main;
   padding-top: 50px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  box-sizing: border-box;
-  height: 100%;
+  background: lightgreen;
+
   
 `
